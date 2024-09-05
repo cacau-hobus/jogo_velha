020798b7dc6d1ea6c47831c91226effde3edca26
Claudia <hobuu_013@outlook.com>
2024-09-04 21:17:17 -0300

    passando parametros para o quadro

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,21 +4,21 @@ export default function () {
     return (
         <div style={{ display: 'flex' }}>
             <div className="board-row">
-                <Square></Square>
-                <Square></Square>
-                <Square></Square>
+                <Square value="1"></Square>
+                <Square value="1"></Square>
+                <Square value="1"></Square>
             </div>
 
             <div className="board-row">
-                <Square></Square>
-                <Square></Square>
-                <Square></Square>
+                <Square value="1"></Square>
+                <Square value="1"></Square>
+                <Square value="1"></Square>
             </div>
 
             <div className="board-row">
-                <Square></Square>
-                <Square></Square>
-                <Square></Square>
+                <Square value="1"></Square>
+                <Square value="1"></Square>
+                <Square value="1"></Square>
             </div>
         </div>
     )
